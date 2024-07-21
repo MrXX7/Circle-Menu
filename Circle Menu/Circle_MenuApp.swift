989c4b00d120657e66503038ba02a352ca4b8b35
Oncu <oohancan@gmail.com>
2024-07-21 21:47:08 +0300

    Initial Commit

---
+++ Circle Menu/Circle_MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  Circle_MenuApp.swift
+//  Circle Menu
+//
+//  Created by Oncu Can on 21.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Circle_MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
