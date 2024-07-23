e87020b5b664ccca8155eaea5c6aac09ac1abc53
Oncu <oohancan@gmail.com>
2024-07-23 12:18:08 +0300

    Add angle calculation and update items in CircleView

--- Circle Menu/Views/CircleView.swift
+++ Circle Menu/Views/CircleView.swift
@@ -12,6 +12,9 @@ struct CircleView: View {
     var distance: CGFloat = 120.0
     var buttonHeight: CGFloat = 55.0
     
+    var startAngle: Double = 0.0
+    var endAngle: Double = 120.0
+    
     @Binding var items: [ItemModel]
     
     var body: some View {
@@ -28,6 +31,33 @@ struct CircleView: View {
                     }
                 }
             }
+            .onAppear {
+                updateItems()
+            }
+    }
+    
+    fileprivate func updateItems() {
+        let step = getStep()
+        
+        for i in 0..<items.count {
+            let angle: Double = startAngle + Double(i) * step
+            
+            items[i].id = i
+            items[i].angle = angle
+        }
+    }
+    
+    fileprivate func getStep() -> Double {
+        var length = endAngle - startAngle
+        var count = items.count
+        
+        if length < endAngle {
+            count -= 1
+        } 
+    else if length > endAngle {
+            length = endAngle
+        }
+        return length / Double(count)
     }
 }
 
